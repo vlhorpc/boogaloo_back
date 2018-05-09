a012a45452c5e4ebb5bb59dbb214fbb3ae47f2f7
volodia.g <volodia.gp@gmail.com>
2018-05-09 18:37:26 +0300

    cors addded

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const routes = require('./routes');
 const mongoose = require('mongoose');
@@ -31,6 +32,7 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
