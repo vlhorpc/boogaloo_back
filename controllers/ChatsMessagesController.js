5b29c09c4c0c6d5dcfed97d1a2154ecefefc1a06
volodia.g <volodia.gp@gmail.com>
2018-07-18 16:03:27 +0300

    get messages list

--- controllers/ChatsMessagesController.js
+++ controllers/ChatsMessagesController.js
@@ -12,7 +12,44 @@ class ChatsMessagesController extends Controller {
   }
 
   getAction() {
-    return this.res.json('getAction');
+    const { req, res } = this;
+    const { userData, urlParams } = req;
+    const { limit, offset } = urlParams;
+
+    models.ChatsUsers.find({
+      where: {
+        user_id: userData.user_id,
+        chat_id: urlParams.chat_id
+      }
+    }).then((currentChat) => {
+      if (currentChat) {
+        models.ChatsMessages
+          .findAll({
+            where: {
+              chat_id: urlParams.chat_id
+            },
+            limit: Number(limit) || 10,
+            offset: Number(offset) || 0,
+            order: [
+              ['createdAt', 'DESC']
+            ]
+          })
+          .then((messages) => {
+            this.setResponseData({ response: messages });
+            return this.returnInformation();
+          })
+          .catch(() => {
+            this.code = 404;
+            return this.returnInformation();
+          });
+      } else {
+        this.code = 404;
+        return this.returnInformation();
+      }
+    }).catch((err) => {
+      this.setResponseData({ code: 422, message: err });
+      return this.returnInformation();
+    });
   }
 
   postAction() {
@@ -31,7 +68,7 @@ class ChatsMessagesController extends Controller {
 
         models.ChatsMessages.create(messageInformation)
           .then((createdMessage) => {
-            this.setResponseData({ code: 404, response: createdMessage });
+            this.setResponseData({ response: createdMessage });
             return this.returnInformation();
           })
           .catch(() => {
