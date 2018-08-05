c1b4f874201dc37ea92911abd51b1191aab1af3f
volodia.g <volodia.gp@gmail.com>
2018-08-05 20:50:36 +0300

    return chat by friend id

--- controllers/ChatsUsersController.js
+++ controllers/ChatsUsersController.js
@@ -9,6 +9,41 @@ class ChatsUsersController extends Controller {
     this.res = res;
   }
 
+  getAction() {
+    const { req } = this;
+    const { userData, urlParams } = req;
+    const { friendId } = urlParams;
+
+    if (friendId) {
+      models.ChatsUsers.findAll({
+        where: {
+          user_id: userData.user_id
+        }
+      }).then((chatsList) => {
+        const idsList = chatsList.map(chat => chat.chat_id);
+
+        models.ChatsUsers.find({
+          where: {
+            user_id: friendId,
+            chat_id: {
+              $in: idsList
+            }
+          }
+        }).then((response) => {
+          if (response) {
+            this.response = response;
+            return this.returnInformation();
+          }
+          this.code = 404;
+          return this.returnInformation();
+        });
+      });
+    } else {
+      this.code = 422;
+      this.returnInformation();
+    }
+  }
+
   putAction() {
     const { req } = this;
     const { body: bodyParams, userData } = req;
--- routes/chatsUsersRouter.js
+++ routes/chatsUsersRouter.js
@@ -4,6 +4,7 @@ const { ChatsUsersController } = require('../controllers');
 const chatsUsersRouter = express.Router();
 
 chatsUsersRouter.route('/')
+  .get((req, res) => (new ChatsUsersController(req, res)).getAction())
   .put((req, res) => (new ChatsUsersController(req, res)).putAction());
 
 module.exports = chatsUsersRouter;
